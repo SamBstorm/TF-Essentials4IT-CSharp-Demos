101f542982c8349c5989a2040899c6c26631bd83
Samuel Legrain <samuel.legrain@bstorm.be>
2023-08-22 11:55:16 +0200

    Update Démo Event

--- Demo-Event/Program.cs
+++ Demo-Event/Program.cs
@@ -1,16 +1,7 @@
 ﻿using Demo_Event;
 
-
-Action<Poisson> OnDeathConsole = (Poisson poisson) => Console.WriteLine($"Oups... A plus {poisson.Name}");
-
-
-/*delegate(Poisson poisson) {
-Console.WriteLine($"Oups... A plus {poisson.Name}");
-};*/
-
 Poisson nemo = new Poisson("Némo");
 nemo.OnDeath += OnDeathConsole;
-nemo.OnDeath += (Poisson p) => p.OnDeath -= OnDeathConsole;
 do
 {
     Console.WriteLine($"{nemo.Name} est toujours là!");
@@ -18,5 +9,8 @@ do
 } while (nemo.EstEnVie);
 nemo.Vieillir();
 
-
-//delegate void ActionSurPoisson(Poisson p);
+void OnDeathConsole(Poisson poisson)
+{
+    poisson.OnDeath -= OnDeathConsole;
+    Console.WriteLine($"Oups... A plus {poisson.Name}");
+}
