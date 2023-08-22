71643be837c5f8d263714c9158b5ccf06179958e
Samuel Legrain <samuel.legrain@bstorm.be>
2023-08-22 10:45:37 +0200

    Ajoutez des fichiers projet.

---
+++ Demo-Event/Poisson.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Event
+{
+    internal class Poisson
+    {
+        private int _pv;
+        public int Pv { 
+            get { return _pv;}
+            private set { 
+                _pv = value;
+                if(_pv <= 0) OnDeath?.Invoke(this);
+            }
+        }
+
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public bool EstEnVie { get { return Pv > 0; } }
+
+        public event Action<Poisson> OnDeath;
+        public Poisson(string name)
+        {
+            Name = name;
+            Pv = 10;
+        }
+
+        public void Vieillir()
+        {
+            Pv--;
+        }
+
+    }
+}
---
+++ Demo-Event/Program.cs
@@ -0,0 +1,22 @@
+﻿using Demo_Event;
+
+
+Action<Poisson> OnDeathConsole = (Poisson poisson) => Console.WriteLine($"Oups... A plus {poisson.Name}");
+
+
+/*delegate(Poisson poisson) {
+Console.WriteLine($"Oups... A plus {poisson.Name}");
+};*/
+
+Poisson nemo = new Poisson("Némo");
+nemo.OnDeath += OnDeathConsole;
+nemo.OnDeath += (Poisson p) => p.OnDeath -= OnDeathConsole;
+do
+{
+    Console.WriteLine($"{nemo.Name} est toujours là!");
+    nemo.Vieillir();
+} while (nemo.EstEnVie);
+nemo.Vieillir();
+
+
+//delegate void ActionSurPoisson(Poisson p);
