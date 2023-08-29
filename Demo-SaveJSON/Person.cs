7e06243337966fbdc93708625033cff6f9517800
Samuel Legrain <samuel.legrain@bstorm.be>
2023-08-29 09:37:10 +0200

    Add Demo Json + SaveFile

---
+++ Demo-SaveJSON/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_SaveJSON
+{
+    internal class Person
+    {
+        public string firstname { get; set; }
+        public string lastname { get; set; }
+        public DateTime birthdate { get; set; }
+    }
+}
---
+++ Demo-SaveJSON/Program.cs
@@ -0,0 +1,54 @@
+﻿using Demo_SaveJSON;
+using System.Text.Json;
+
+
+//Création de mes objets
+List<Person> profs = new List<Person>();
+
+Person p1 = new Person()
+{
+    firstname = "Samuel",
+    lastname = "Legrain",
+    birthdate = new DateTime(1987, 9, 27)
+};
+
+Person p2 = new Person()
+{
+    firstname = "Alexandre",
+    lastname = "Claes",
+    birthdate = new DateTime(1987, 9, 27)
+};
+
+profs.Add(p1);
+profs.Add(p2);
+
+
+//Convertion objects => text format JSON
+string json_result = JsonSerializer.Serialize(profs);
+Console.WriteLine(json_result);
+
+//Sauvegarde du text sur fichier
+//Définition du chemin d'accès au fichier
+DirectoryInfo cDirs = Directory.CreateDirectory(@"c:\Save\Test");
+
+//Définition du fichier à l'aide du stream writer
+using (StreamWriter sw = new StreamWriter($"{cDirs.FullName}\\save.json"))
+{
+    sw.WriteLine(json_result);
+}
+
+//Relecture du fichier
+string save_json;
+using (StreamReader sr = new StreamReader($"{cDirs.FullName}\\save.json"))
+{
+    save_json = sr.ReadToEnd();
+}
+
+//Convertion text format JSON => objects
+
+List<Person> convert_result = JsonSerializer.Deserialize<List<Person>>(save_json);
+
+foreach (Person p in convert_result)
+{
+    Console.WriteLine($"{p.firstname} , {p.lastname}");
+}
